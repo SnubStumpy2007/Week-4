9183ac3e59d12c63b890ecbc07554958dd553389
SnubStumpy2007 <joshua192007@gmail.com>
2023-05-20 14:46:27 -0400

    building initial JavaScript

---
+++ script.js
@@ -0,0 +1,19 @@
+// ideas
+// https://www.codingnepalweb.com/quiz-app-with-timer-javascript/
+
+// define a constant object called questions.
+// This object contains various questions and answers in the form of arrays.
+// Arrays can even be housed in objects.
+// Arrays can have other arrays nested in them as elements.
+const questions = [
+    {
+    question: "What type of variable cannot be changed later in the code?",
+    options: ['const', 'let', 'var', 'double', 'string'],
+    answer: 'const' 
+    },
+    {
+    question: "What is an array?",
+    options: ['const', 'let', 'var', 'double', 'string'],
+    answer: 'const' 
+    }
+]
