91b54d7c73898c02cf3d6f6874c831ab0b477ec5
SnubStumpy2007 <joshua192007@gmail.com>
2023-05-20 15:06:16 -0400

    finished questions object and nested arrays in the JavaScript file.

--- script.js
+++ script.js
@@ -13,7 +13,22 @@ const questions = [
     },
     {
     question: "What is an array?",
-    options: ['const', 'let', 'var', 'double', 'string'],
-    answer: 'const' 
+    options: ['Assigns a value to a variable', 'A power grid', 'A variable that changes at the end of the program', 'A variable that holds many properties', 'A variable that holds more than one value.'],
+    answer: 'A variable that holds more than one value.' 
+    },
+    {
+    question: "What is an object?",
+    options: ['Assigns a value to a variable', 'A power grid', 'A variable that changes at the end of the program', 'A variable that holds many properties', 'A variable that holds more than one value.'],
+    answer: 'A variable that holds many properties.' 
+    },
+    {
+    question: "What is a method?",
+    options: ['Method and Red, a tv show from the mid-2000s', 'A power grid', 'A shorter syntax for defining a function property in the initalizer', 'A variable that holds many properties', 'A string of text.'],
+    answer: 'A shorter syntax for defining a function property in the initalizer.' 
+    },
+    {
+    question: "What is a function?",
+    options: ['An exercise at the gym', 'A block of code designed to perform a task', 'Creates a health bar in a video game', 'A variable that can be changed later in the code', 'A variable that holds more than one value.'],
+    answer: 'A block of code designed to perform a task.' 
     }
 ]
